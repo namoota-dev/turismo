1a5ce0798f0f3643b0957202286a1e4253c1e588
zwelisha <zwescientist@gmail.com>
2024-11-21 01:29:54 +0200

    Changed footer and colours

---
+++ components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white py-3">
+      <Container className="text-center">
+        <p>&copy; 2024 Turismo. All rights reserved.</p>
+      </Container>
+    </footer>
+  );
+}
---
+++ components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+export default function Header() {
+  return (
+    <Navbar id="navbarTop" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="/">
+            <Image src="/logo.jpeg" rounded className="img-fluid" width={100}/>   
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#destinations">Destinations</Nav.Link>
+            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
